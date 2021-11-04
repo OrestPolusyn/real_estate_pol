8572da7aee015f47e35412bcf2337b97f41d4d67
opect.pol@i.ua <opect.pol@i.ua>
2021-11-05 00:38:58 +0200

    fix validation

--- src/js/components/validation.js
+++ src/js/components/validation.js
@@ -3,79 +3,53 @@ import intlTelInput from 'intl-tel-input';
 import utils from '../vendor/utils'
 
 
-const formName = Array.from(vars.$formName),
-  formMail = Array.from(vars.$formEmail),
-  formTel = Array.from(vars.$formTel),
-  formCkeck = Array.from(vars.$formCheck),
-  modalClose = Array.from(vars.$closeModal),
-  openModalsBtn = Array.from(vars.$openModals);
+const modalClose = Array.from(vars.$closeModal);
 
 
-const iti = intlTelInput(vars.$formTel, {
-  preferredCountries: ["ua"],
-  utilsScript: "utils",
-});
+if (document.querySelector('.contact-form')) {
+  const iti = intlTelInput(vars.$formTel, {
+    preferredCountries: ["ua"],
+    utilsScript: "utils",
+  });
+
+  vars.$formSend.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    if (vars.$formName.value.length === 0) {
+      document.querySelector('[data-target="name"]').parentNode.classList.add('error');
+    } else {
+      document.querySelector('[data-target="name"]').parentNode.classList.remove('error');
+    };
+
+    if (!vars.$formEmail.value.includes("@")) {
+      document.querySelector('[data-target="email"]').parentNode.classList.add('error');
+    } else {
+      document.querySelector('[data-target="email"]').parentNode.classList.remove('error');
+    };
+
+    if (!iti.isValidNumber()) {
+      document.querySelector(`[data-target="tel"]`).parentNode.classList.add('error');
+    } else {
+      document.querySelector(`[data-target="tel"]`).parentNode.classList.remove('error');
+    };
+
+    if (!vars.$formCheck.checked) {
+      document.querySelector('[data-target="ckeck"]').parentNode.classList.add('error');
+    } else {
+      document.querySelector('[data-target="ckeck"]').parentNode.classList.remove('error');
+    };
+
+    if (vars.$formName.value.length > 0 && iti.isValidNumber() && vars.$formCheck.checked && vars.$formEmail.value.includes("@")) {
+      openModal(e);
+    } else {
+      return false;
+    };
+  });
+}
 
 const itiModal = intlTelInput(vars.$formTelModal, {
   preferredCountries: ["ua"],
   utilsScript: "utils",
-});
-
-if (modalClose.length > 0) {
-  modalClose.forEach(element => {
-    element.addEventListener('click', () => {
-      document.querySelector('.modal').classList.remove('modal--open');
-      document.querySelectorAll('.modal__content').forEach(element => {
-        element.classList.remove('modal__content--open');
-      });
-    });
-  });
-};
-
-vars.$headerBtnModal.addEventListener('click', function (e) {
-  let btnPath = e.currentTarget.getAttribute('data-path');
-
-  document.querySelector(`[data-target="${btnPath}"]`).classList.add('modal__content--open');
-
-  document.querySelector('.modal').classList.add('modal--open');
-
-  if (e.currentTarget.getAttribute('data-path') === 'modal-finish') {
-    document.querySelector('[data-target=modal-form]').classList.remove('modal__content--open');
-  }
-});
-
-vars.$formSend.addEventListener('submit', function (e) {
-  e.preventDefault();
-
-  if (vars.$formName.value.length === 0) {
-    document.querySelector('[data-target="name"]').parentNode.classList.add('error');
-  } else {
-    document.querySelector('[data-target="name"]').parentNode.classList.remove('error');
-  };
-
-  if (!vars.$formEmail.value.includes("@")) {
-    document.querySelector('[data-target="email"]').parentNode.classList.add('error');
-  } else {
-    document.querySelector('[data-target="email"]').parentNode.classList.remove('error');
-  };
-
-  if (!iti.isValidNumber()) {
-    document.querySelector(`[data-target="tel"]`).parentNode.classList.add('error');
-  } else {
-    document.querySelector(`[data-target="tel"]`).parentNode.classList.remove('error');
-  };
-
-  if (!vars.$formCheck.checked) {
-    document.querySelector('[data-target="ckeck"]').parentNode.classList.add('error');
-  } else {
-    document.querySelector('[data-target="ckeck"]').parentNode.classList.remove('error');
-  };
-
-  if (vars.$formName.value.length > 0 && iti.isValidNumber() && vars.$formCheck.checked && vars.$formEmail.value.includes("@")) {
-    openModal(e);
-  } else {
-    return false;
-  };
 });
 
 vars.$formSendModal.addEventListener('submit', function (e) {
@@ -106,6 +80,31 @@ vars.$formSendModal.addEventListener('submit', function (e) {
   };
 });
 
+
+vars.$headerBtnModal.addEventListener('click', function (e) {
+  let btnPath = e.currentTarget.getAttribute('data-path');
+
+  document.querySelector(`[data-target="${btnPath}"]`).classList.add('modal__content--open');
+
+  document.querySelector('.modal').classList.add('modal--open');
+
+  if (e.currentTarget.getAttribute('data-path') === 'modal-finish') {
+    document.querySelector('[data-target=modal-form]').classList.remove('modal__content--open');
+  }
+});
+
+
+
+if (modalClose.length > 0) {
+  modalClose.forEach(element => {
+    element.addEventListener('click', () => {
+      document.querySelector('.modal').classList.remove('modal--open');
+      document.querySelectorAll('.modal__content').forEach(element => {
+        element.classList.remove('modal__content--open');
+      });
+    });
+  });
+};
 
 function openModal(e) {
   let btnPath = e.submitter.getAttribute('data-path');
