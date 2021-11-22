4b704a75c12457bf0bf627000e2cc8e4bbdf119a
Orest <opect.pol@i.ua>
2021-11-22 13:27:03 +0200

    fix unscroll

--- src/js/main.js
+++ src/js/main.js
@@ -39,15 +39,3 @@ if (storyBtn || homeContact) {
     window.scrollTo(0, document.querySelector('.contacts').offsetTop - 100);
   });
 }
-
-document.addEventListener('touchmove', function (event) {
-  if (event.scale !== 1) { event.preventDefault(); }
-}, { passive: false });
-
-if (/iPad|iPhone|iPod/.test(navigator.userAgent)) {
-  window.document.addEventListener('touchmove', e => {
-    if(e.scale !== 1) {
-      e.preventDefault();
-    }
-  }, {passive: false});
-}
